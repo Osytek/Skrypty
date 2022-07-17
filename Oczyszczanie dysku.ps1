9630571ce06dc32322abe633f10bf5139ac9fd33
[OsytekRadoslaw] <[osyteczek@gmail.com]>
2022-07-17 23:08:40 +0200

    initial commit

---
+++ Oczyszczanie dysku.ps1
@@ -0,0 +1,24 @@
+﻿$HKLM = [UInt32] “0x80000002”
+$strKeyPath = “SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches”
+$strValueName = “StateFlags0065”
+
+$subkeys = gci -Path HKLM:\$strKeyPath -Name
+ForEach ($subkey in $subkeys) {
+Try {
+New-ItemProperty -Path HKLM:\$strKeyPath\$subkey -Name $strValueName -PropertyType DWord -Value 2 -ErrorAction SilentlyContinue| Out-Null
+}
+Catch {
+}
+try {
+Start-Process cleanmgr -ArgumentList “/sagerun:65” -Wait -NoNewWindow -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
+}
+catch {
+}
+}
+ForEach ($subkey in $subkeys) {
+Try {
+Remove-ItemProperty -Path HKLM:\$strKeyPath\$subkey -Name $strValueName | Out-Null
+}
+Catch {
+}
+}
---
+++ Optymalizacja dysku.ps1
@@ -0,0 +1,1 @@
+﻿Optimize-Volume -DriveLetter C
